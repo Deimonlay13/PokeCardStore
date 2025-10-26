19fbae01f96c8de201dac4bdf55dcf3abafb40ef
GeraldineBecerra <gera.becerra@duocuc.cl>
2025-10-26 04:19:36 -0300

    Se agrega modal lateral derecho  y estados para el carrito de compras

--- src/shared/component/Header.tsx
+++ src/shared/component/Header.tsx
@@ -1,11 +1,16 @@
-import { Navbar, Nav, Container } from "react-bootstrap";
+import { Navbar, Nav, Container, Offcanvas } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { useCart } from "../../cartas/context/CartContext";
+import { useState } from "react";
+import { CartDetails } from "../../cartas/components/CartDetails";
 
 const Header = () => {
+    const { totalItems } = useCart();
+      const [showCart, setShowCart] = useState(false);
+        const handleClose = () => setShowCart(false);
   return (
     <Navbar bg="dark" variant="dark" expand="lg" sticky="top">
       <Container>
-
         <Navbar.Brand as={NavLink} to="/">
           PokecardStore
         </Navbar.Brand>
@@ -28,7 +33,26 @@ const Header = () => {
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
+        <button
+          className="btn btn-outline-primary position-relative"
+          onClick={() => setShowCart(!showCart)}
+        >
+          🛒 Carrito
+          {totalItems > 0 && (
+            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+              {totalItems}
+            </span>
+          )}
+        </button>
       </Container>
+      <Offcanvas show={showCart} placement="end" onHide={handleClose}>
+        <Offcanvas.Header closeButton>
+          <Offcanvas.Title>Tu carrito 🛒</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <CartDetails />
+        </Offcanvas.Body>
+      </Offcanvas>
     </Navbar>
   );
 };
