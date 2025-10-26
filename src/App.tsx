2ead5c39848275c5b35d40264f7b1f97ddfceb65
Deimonlay13 <di.pizarrom@duocuc.cl>
2025-10-26 17:11:17 -0300

    resolviendo conflictos

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
-<<<<<<< HEAD
+
 
 import './App.css'
 import { Formulario } from './formulario/components/Formulario'
 
-function App() {
 
-  return (
-    <>
-      <Formulario/>
-    </>
-  )
-=======
+
 import { RouterProvider } from 'react-router-dom';
 import './App.css';
 import { appRouter } from './router/app.router';
 
 function App() {
   return <RouterProvider router={appRouter} />;
->>>>>>> origin/dev
+    <>
+      <Formulario/>
+    </>
+
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,10 @@ import './index.css'
 import App from './App'
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
 import "bootstrap/dist/css/bootstrap.min.css";
-=======
-import "bootstrap/dist/css/bootstrap.min.css";
 import { CartProvider } from './cartas/context/CartContext';
->>>>>>> origin/dev
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
