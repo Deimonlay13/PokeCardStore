7a98590f213fb7cc57f50767d6ac4b6d082afddf
GeraldineBecerra <gera.becerra@duocuc.cl>
2025-10-26 23:06:08 -0300

    se modifica estructura para los test

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -1,14 +1,16 @@
-// __tests__/sum.test.ts
-function suma(a: number, b: number) {
-  return a + b;
-}
+import { render } from "@testing-library/react";
+import App from "../src/App";
+import { CartProvider } from "../src/cartas/context/CartContext";
 
-describe("Prueba de suma", () => {
-  it("suma 2 + 3 y da 5", () => {
-    expect(suma(2, 3)).toBe(5);
+describe("App Component", () => {
+  it("se renderiza sin errores", () => {
+    const container = render(
+      <CartProvider>
+        <App />
+      </CartProvider>
+    ).container;
+
+    expect(container).toBeDefined();
   });
 
-  it("suma números negativos", () => {
-    expect(suma(-2, -3)).toBe(-5);
-  });
 });
--- karma.conf.cjs
+++ karma.conf.cjs
@@ -2,36 +2,74 @@ const path = require("path");
 
 module.exports = function (config) {
   config.set({
+    basePath: "",
     frameworks: ["jasmine"],
-    files: ["__tests__/**/*.ts", "__tests__/**/*.tsx"],
+
+    files: [
+      { pattern: "__tests__/**/*.test.ts", watched: false },
+      { pattern: "__tests__/**/*.test.tsx", watched: false },
+      { pattern: "src/**/*.spec.ts", watched: false },
+      { pattern: "src/**/*.spec.tsx", watched: false },
+    ],
+
     preprocessors: {
-      "__tests__/**/*.ts": ["webpack"],
-      "__tests__/**/*.tsx": ["webpack"],
+      "__tests__/**/*.test.ts": ["webpack", "sourcemap"],
+      "__tests__/**/*.test.tsx": ["webpack", "sourcemap"],
+      "src/**/*.spec.ts": ["webpack", "sourcemap"],
+      "src/**/*.spec.tsx": ["webpack", "sourcemap"],
     },
+
     webpack: {
       mode: "development",
+      devtool: "inline-source-map",
       resolve: {
         extensions: [".ts", ".tsx", ".js", ".jsx"],
       },
       module: {
         rules: [
           {
-            test: /\.(ts|tsx)$/,
-            use: {
-              loader: "ts-loader",
-              options: { configFile: "tsconfig.karma.json" },
-            },
+            test: /\.[jt]sx?$/,
             exclude: /node_modules/,
+            use: {
+              loader: "babel-loader",
+              options: {
+                presets: [
+                  "@babel/preset-env",
+                  ["@babel/preset-react", { runtime: "automatic" }],
+                  "@babel/preset-typescript",
+                ],
+                plugins: ["babel-plugin-istanbul"],
+              },
+            },
           },
           {
-            test: /\.css$/,
+            test: /\.css$/i,
             use: ["style-loader", "css-loader"],
           },
         ],
       },
     },
-    reporters: ["progress"],
-    browsers: ["Chrome"],
-    singleRun: false,
+
+    reporters: ["progress", "coverage"],
+
+    coverageReporter: {
+      dir: path.join(__dirname, "coverage"),
+      reporters: [
+        { type: "html", subdir: "html", includeAllSources: true },
+        { type: "lcovonly", subdir: ".", file: "lcov.info" },
+        { type: "text-summary" },
+      ],
+    },
+
+    browsers: ["ChromeHeadless"],
+    customLaunchers: {
+      ChromeHeadlessCustom: {
+        base: "ChromeHeadless",
+        flags: ["--no-sandbox", "--disable-gpu"],
+      },
+    },
+
+    singleRun: true,
+    concurrency: Infinity,
   });
 };
